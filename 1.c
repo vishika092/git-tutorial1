938405c3c89bbaa093f4bbfbe1ed6b631bfd4e46
vishika092 <vishikajain0@gmail.com>
2024-01-27 14:10:01 +0530

    committed 5

--- 1.c
+++ 1.c
@@ -3,8 +3,9 @@
 int main(){
     int a = 10, b= 22;
     
-
-    printf("%d\n", 10);
+    printf("%s\n", "hello this is first file");
+    printf("%d\n", a);
+    printf("%d\n", b);
     return 0;
 }
 
